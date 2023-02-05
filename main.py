b76c8da9ece0cd24a71bf6965650e61b01d35aca
harsi <harsimransidhu770@gmail.com>
2023-02-05 18:01:34 -0400

    Working Code 1.0

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
 
-# Set options for the webdriver
-options = Options()
-options.add_argument("--user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0'")
-
-# Initialize a Selenium webdriver
+# Initialize a webdriver instance
 driver = webdriver.Firefox()
 
-# Visit the Indeed website
+# Navigate to Indeed.com
 driver.get("https://www.indeed.com/")
 
-# Find the search bar and enter a job search query
-search_bar = driver.find_element(By.NAME, "q")
-search_bar.send_keys("data scientist")
+# Wait for the search bar element to appear and enter your search query
+search_bar = WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.ID, "text-input-what")))
+search_bar.send_keys("Software Engineer")
 
-# Submit the search query
-search_bar.submit()
+# Find the search button and click it
+search_button = driver.find_element(By.CLASS_NAME,"yosegi-InlineWhatWhere-primaryButton")
+search_button.click()
 
-# Wait for the page to load
-driver.implicitly_wait(10)
+# Use Beautiful Soup to parse the HTML content of the page
+soup = BeautifulSoup(driver.page_source, "html.parser")
 
-# Scrape the job listings
-html = driver.page_source
-soup = BeautifulSoup(html, "html.parser")
-results_list = soup.find("ul", class_="jobsearch-ResultsList css-0")
-job_listings_title = results_list.find_all("li", class_="jobsearch-SerpJobCard")
+# Find all job listings on the page
+job_listings = soup.find_all("div", class_="job_seen_beacon")
 
-# Close the browser
+# Extract relevant information from each job listing
+for job in job_listings:
+    title = job.find("span", attrs={"id": lambda x: x and x.startswith("jobTitle-")})
+    title = title.text
+    company = job.find("span", class_="companyName").text
+    location = job.find("div", class_="companyLocation").text
+    print(f"{title} at {company} in {location}")
+
+# Close the webdriver instance
 driver.quit()

b6c1fd862647e1228ac57498ede06d754832a101
harsi <harsimransidhu770@gmail.com>
2023-02-06 20:26:59 -0400

    working with click library

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-import argparse
-from bs4 import BeautifulSoup
+import click
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 
+DRIVER_CLASSES = {
+    'Firefox': webdriver.Firefox,
+    'Chrome': webdriver.Chrome,
+    'Safari': webdriver.Safari,
+    'Edge': webdriver.Edge,
+}
 
-def extract_job_info(soup):
+@click.command()
+@click.option('--driver', type=click.Choice(DRIVER_CLASSES.keys()), default='Firefox', help='WebDriver to use')
+@click.option('--search-query', prompt='Enter the search query', help='The query to search for on indeed.com')
+def scrape_indeed(driver, search_query):
+    DriverClass = DRIVER_CLASSES[driver]
+    driver = DriverClass()
+    driver.get("https://www.indeed.com/")
+    search_bar = WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.ID, "text-input-what")))
+    search_bar.send_keys(search_query)
+    search_button = driver.find_element(By.CLASS_NAME, "yosegi-InlineWhatWhere-primaryButton")
+    search_button.click()
+    soup = BeautifulSoup(driver.page_source, "html.parser")
     job_listings = soup.find_all("div", class_="job_seen_beacon")
     for job in job_listings:
         title = job.find("span", attrs={"id": lambda x: x and x.startswith("jobTitle-")})
@@ -14,37 +31,7 @@ def extract_job_info(soup):
         company = job.find("span", class_="companyName").text
         location = job.find("div", class_="companyLocation").text
         print(f"{title} at {company} in {location}")
-
-
-def main(args):
-    # Initialize the specified webdriver instance
-    if args.webdriver == "firefox":
-        driver = webdriver.Firefox()
-    elif args.webdriver == "chrome":
-        driver = webdriver.Chrome()
-    else:
-        raise ValueError("Invalid webdriver type. Choose either 'firefox' or 'chrome'.")
-
-    driver.get("https://www.indeed.com/")
-
-    search_bar = WebDriverWait(driver, 40).until(
-        EC.presence_of_element_located((By.ID, "text-input-what"))
-    )
-    search_bar.send_keys(args.search_query)
-
-    search_button = driver.find_element(By.CLASS_NAME, "yosegi-InlineWhatWhere-primaryButton")
-    search_button.click()
-
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    extract_job_info(soup)
-
     driver.quit()
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Scrape job information from Indeed.")
-    parser.add_argument("search_query", type=str, help="Search query to send to Indeed.")
-    parser.add_argument("webdriver", type=str, help="Type of webdriver to use.",
-                        choices=["firefox", "chrome"])
-    args = parser.parse_args()
-    main(args)
+if __name__ == '__main__':
+    scrape_indeed()

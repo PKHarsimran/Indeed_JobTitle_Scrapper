6fe52fd12cf6d6870406ad95a75ef6a174087680
harsi <harsimransidhu770@gmail.com>
2023-02-07 15:12:20 -0400

    Fun

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Indeed_JobTitle_Scrapper
-This code uses Selenium and Beautiful Soup to scrape job listings information from Indeed.com. It opens a webdriver, enters a search query, clicks the search button, and uses Beautiful Soup to parse the HTML content. The script extracts relevant information, including job title, company, and location, and outputs it to the console.
+This code is a simple job scraper that uses the selected webdriver (Firefox, Chrome, Safari, or Edge) to scrape job information from indeed.com. The user can specify the search query and location and the code will scrape job data for the given query in the specified location. The scraped data is then saved in an Excel workbook with the name 'job_data.xlsx' and the sheet name is the search query.
+Prerequisites
+
+    Python 3.x
+    Selenium
+    Openpyxl
+    Pandas
+    BeautifulSoup
+    Click
+
+The scraped data will be saved in an Excel workbook named job_data.xlsx in the project directory.
+Conclusion
+
+This code is a simple job scraper that can be useful for job seekers who want to scrape job data from indeed.com for their desired job search query and location. The code can be easily modified to scrape job data from other job websites as well.
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import click
+import openpyxl
+import pandas as pd
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+
 
 # Dictionary of available webdriver classes
 DRIVER_CLASSES = {
@@ -19,7 +23,8 @@ DRIVER_CLASSES = {
 @click.option('--driver', type=click.Choice(DRIVER_CLASSES.keys()), prompt='Select the webdriver to use', help='WebDriver to use')
 # Add an option for user to provide search query
 @click.option('--search-query', prompt='Enter the search query', help='The query to search for on indeed.com')
-def scrape_indeed(driver, search_query):
+@click.option('--search-location', prompt='Enter the location eg - Any or Remote', help='Location to search from indeed.com. Format: City, Country or Remote')
+def scrape_indeed(driver, search_query, search_location):
     """
     This function scrapes job information from indeed.com using the selected webdriver.
     """
@@ -33,6 +38,12 @@ def scrape_indeed(driver, search_query):
     search_bar = WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.ID, "text-input-what")))
     # Enter the search query into the search bar
     search_bar.send_keys(search_query)
+    #Wait for Location search bar to load
+    location_bar = WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.ID, "text-input-where")))
+    # Enter the Location query into the Location bar
+    location_bar.send_keys(Keys.CONTROL + "a")
+    location_bar.send_keys(Keys.BACKSPACE)
+    location_bar.send_keys(search_location)
     # Find and click the search button
     search_button = driver.find_element(By.CLASS_NAME, "yosegi-InlineWhatWhere-primaryButton")
     search_button.click()
@@ -40,6 +51,17 @@ def scrape_indeed(driver, search_query):
     soup = BeautifulSoup(driver.page_source, "html.parser")
     # Find all job listings on the page
     job_listings = soup.find_all("div", class_="job_seen_beacon")
+
+    # Create an empty dataframe to store job information
+    job_data = pd.DataFrame(columns=['Title', 'Company', 'Location'])
+
+    # Create a new Excel file if it doesn't already exist
+    try:
+        workbook = openpyxl.load_workbook("job_data.xlsx")
+    except FileNotFoundError:
+        workbook = openpyxl.Workbook()
+        workbook.save("job_data.xlsx")
+
     # Print the title, company, and location for each job listing
     for job in job_listings:
         title = job.find("span", attrs={"id": lambda x: x and x.startswith("jobTitle-")})
@@ -47,7 +69,18 @@ def scrape_indeed(driver, search_query):
         company = job.find("span", class_="companyName").text
         location = job.find("div", class_="companyLocation").text
         print(f"{title} at {company} in {location}")
-    # Close the webdriver
+        temp_df = pd.DataFrame({'Title': [title], 'Company': [company], 'Location': [location]})
+        job_data = pd.concat([job_data, temp_df], ignore_index=True)
+
+    # Write the data to an Excel workbook
+    try:
+        writer = pd.ExcelWriter('job_data.xlsx', engine='openpyxl', mode='a', if_sheet_exists='replace')
+    except ValueError:
+        writer = pd.ExcelWriter('job_data.xlsx', engine='openpyxl', mode='a', if_sheet_exists='fail')
+        print("Sheet with that name already exists.")
+    job_data.to_excel(writer, sheet_name=search_query, index=False)
+    writer.close()
+
     driver.quit()
 
 if __name__ == '__main__':
